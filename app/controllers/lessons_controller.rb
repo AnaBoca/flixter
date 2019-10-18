c388b165e9a1dfe3475e553d1fc050e10bf36233
Ana Boca <Ana.Boca@asu.edu>
2019-10-18 20:04:50 +0000

    Lesson 28 - Permissions

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,7 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_enrollment_for_current_lesson, only: [:show]
+
   def show
   end
 
@@ -7,5 +10,11 @@ class LessonsController < ApplicationController
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
+  end
+
+  def require_enrollment_for_current_lesson
+    if !current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to course_path(current_lesson.section.course), alert: "Enroll to access this lesson."
+    end
   end
 end
